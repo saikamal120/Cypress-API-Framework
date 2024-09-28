a3b3542901b7261923bb0d1f62d46016a8d8b9a7
saikamal120 <102736640+saikamal120@users.noreply.github.com>
2024-09-28 14:53:05 +0530

    Add files via upload

---
+++ cypress/e2e/e2eApiCallsUsingChaining.cy.ts
@@ -0,0 +1,43 @@
+import * as callsOnOrders from "../support/orderObjects";
+import {APIRequest, submitOrder} from "../support/orderObjects";
+
+describe('Simple Books API with list of books, new orders, update orders and delete orders', () => {
+    var authToken: any;
+    var orderNumber1: any;
+    var orderNumber2: any;
+    var invaldOrderNumber: 'qwerty';
+
+    before(("capture access token"), function () {
+        //creating new token every time the script runs and pass token to next textcases
+        const apiToken = new APIRequest();
+        apiToken.makeLoginRequest().then(function ($token) {
+            authToken = $token;
+        })
+    })
+    function getRandomNumber(min: number, max: number) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    it(("Complex successful test case to submit three Orders, get all orders then update third order, then Get it, then delete second order and then Get all remaining orders"), () => {
+    // Submit an order and fetch oder details and pass to next testcases
+    var orderNumber1: any;
+    var orderNumber2: any;
+    let cypressTestUser = 'CypressTestUser_';
+    const orderNum1 = new submitOrder(authToken,'1', cypressTestUser+ getRandomNumber(1,10),201)
+    const orderNum2 = new submitOrder(authToken,'1',cypressTestUser+ getRandomNumber(1,10),201)
+    const orderNum3 = new submitOrder(authToken,'1',cypressTestUser+ getRandomNumber(1,10),201)
+    orderNum1.makeOrderSubmit().then(function ($order) {
+        orderNumber1 = $order;
+        orderNum2.makeOrderSubmit().then(function ($order) {
+            orderNumber2 = $order;
+            orderNum3.makeOrderSubmit().then(function ($order) {
+                callsOnOrders.getAllOrders(authToken);
+                callsOnOrders.updatingAnOrder(authToken, $order, 'Sam', 204);
+                callsOnOrders.getOrder(authToken, $order, 200);
+            })
+            callsOnOrders.deleteAnOrder(authToken, orderNumber2, 204);
+            callsOnOrders.getAllOrders(authToken);
+        })
+    })
+    })
+
+})
