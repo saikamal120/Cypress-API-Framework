5f510f4862476bdce5f0bfd125d49ce3f07095de
saikamal120 <102736640+saikamal120@users.noreply.github.com>
2024-09-28 14:32:20 +0530

    Update e2eApiCalls.cy.ts

--- cypress/e2e/e2eApiCalls.cy.ts
+++ cypress/e2e/e2eApiCalls.cy.ts
@@ -7,7 +7,7 @@ describe('Simple Books API with list of books, new orders, update orders and del
         var authToken: any;
         var orderNumber1: any;
         var orderNumber2: any;
-        var orderNumber3: 'qwerty';
+        var invaldOrderNumber: 'qwerty';
 
        before(("capture access token"), function () {
             //creating new token every time the script runs and pass token to next textcases
@@ -17,30 +17,35 @@ describe('Simple Books API with list of books, new orders, update orders and del
             })
        })
        it('List of books', () => {
-        //List of books available in API
-        callsOnBooks.getListOfBooks(200,6);
+            //List of books available in API
+            callsOnBooks.getListOfBooks(200,6);
        })
        it('Get a single book', () => {
-        //get single book information based on bookid
-        callsOnBooks.getSingleBook(1,200);
+            //get single book information based on bookid
+            callsOnBooks.getSingleBook(1,200);
        })
        it('Error on exceeding the list ', () => {
-        //Negitive - error upon exceeding limit of book list
-        callsOnBooks.getSingleBook(7,404);
+            //Negitive - error upon exceeding limit of book list
+            callsOnBooks.getSingleBook(7,404);
        })
        it(("Submit an Order, orderNumber1"), () => {
             // Submit an order and fetch oder details and pass to next testcases
-           const orderNum = new submitOrder(authToken,'1','RAM')
-           orderNum.makeOrderSubmit().then(function ($order) {
+            const orderNum = new submitOrder(authToken,'1','RAM',201)
+            orderNum.makeOrderSubmit().then(function ($order) {
                orderNumber1 = $order;
-           })
+            })
        })
        it(("Submit an Order, orderNumber2"), () => {
             // Submit an order and fetch oder details and pass to next testcases
-            const orderNum = new submitOrder(authToken,'1','ROM')
+            const orderNum = new submitOrder(authToken,'1','ROM',201)
             orderNum.makeOrderSubmit().then(function ($order) {
-            orderNumber2 = $order;
+                orderNumber2 = $order;
             })
+       })
+       it(("Submit an Order, invalid bookId"), () => {
+            // Negative - Submit an order by invalid bookId
+            const orderNum = new submitOrder(authToken,'10','ROM',400)
+            orderNum.makeOrderSubmit();
        })
        it('Get all orders', () => {
             // Get all orders
@@ -48,26 +53,31 @@ describe('Simple Books API with list of books, new orders, update orders and del
        })
        it('Get an order, order number 1', () => {
             //Getting an order response
-            callsOnOrders.getOrder(authToken, orderNumber1, 'RAM',200);
+            callsOnOrders.getOrder(authToken, orderNumber1,200);
        })
        it('Get an order, order number 2', () => {
              //Getting an order response
-             callsOnOrders.getOrder(authToken, orderNumber2, 'ROM',200);
+             callsOnOrders.getOrder(authToken, orderNumber2,200);
        })
-       it('Update an order', () => {
+       it('Get an order, invalid order', () => {
+             //Negative - Getting an invalid order
+             callsOnOrders.getOrder(authToken, invaldOrderNumber,404);
+       })
+       it('Update an order, with valid ordernumber2', () => {
             callsOnOrders.updatingAnOrder(authToken, orderNumber2, 'ROCK',204);
        })
        it('Update an order, with invalid order number', () => {
-            callsOnOrders.updatingAnOrder(authToken, orderNumber3, 'ROCK',404);
+            // Negative case on invalid order
+            callsOnOrders.updatingAnOrder(authToken, invaldOrderNumber, 'ROCK',404);
        })
        it('Get an order after update', () => {
-            callsOnOrders.getOrder(authToken, orderNumber2, 'ROCK',200);
+            callsOnOrders.getOrder(authToken, orderNumber2,200);
        })
        it('Delete order after update', () => {
             callsOnOrders.deleteAnOrder(authToken, orderNumber2,204);
        })
        it('Delete order that is already deleted', () => {
-            // Negitive case on delete call
+            // Negative case on delete call
             callsOnOrders.deleteAnOrder(authToken, orderNumber2,404);
        })
     })
