78c1228d8d4b74585f096530879b52cfa3b114bc
saikamal120 <102736640+saikamal120@users.noreply.github.com>
2024-10-14 19:36:29 +0530

    Update booksObjects.ts

--- cypress/support/booksObjects.ts
+++ cypress/support/booksObjects.ts
@@ -1,23 +1,24 @@
-
 // @ts-ignore
 export const getListOfBooks = (status, length) => {
     cy.request({
         method: 'GET',
-        url: Cypress.env("BOOKS_BASE_URL"),
+        url: Cypress.env("URL") + "/books",
     }).then((response) =>{
         expect(response.status).to.eq(status);
         expect(response.body).has.length(length);
         cy.log('Response body structure:', JSON.stringify(response.body, null, 2))
+        cy.log(response.body[0].name)
     })
 }
 // @ts-ignore
 export const getSingleBook = (bookid, status) => {
     cy.request({
         method: 'GET',
-        url: Cypress.env("BOOKS_BASE_URL")+bookid,
+        url: Cypress.env("URL") + "/books" + "/" + bookid,
         failOnStatusCode: false
     }).then((response) =>{
         expect(response.status).to.eq(status);
         cy.log('Response body structure:', JSON.stringify(response.body, null, 2))
+        cy.log(response.body.name)
     })
 }
